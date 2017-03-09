4f353066bf30fc9139a3a159f8af4fb1e239f799
Tendai Mutunhire <tmutunhire@gmail.com>
2017-03-09 17:48:03 +0200

    get rockin

--- web/static/components/my-app.vue
+++ web/static/components/my-app.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="my-app">
     <h1>FarmVille Chat</h1>
+    <ul v-for="message in messages">
+      <li>{{message}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        messages: ["How are you", "Let's get rockin!"]
+      }
+    }
+
+  }
 </script>
 
 <style lang="sass">
